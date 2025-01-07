fd0c7e8833877270d45e67b35c8e3de52e1f9cb5
borkarsaish65 <borkarsaish65@gmail.com>
2025-01-07 11:14:22 +0530

    savepoint

--- main.js
+++ main.js
@@ -15,12 +15,21 @@ try {
 
 // Function to execute a Python script
 const executePythonScript = (operation) => {
-  console.log(`Starting: ${operation.name}`);
-  
+  console.log(`Starting Python script: ${operation.name}`);
+
   // Prepare arguments for the Python script
-  const scriptArgs = [operation.script, '--env', operation.env, '--programFile' , operation.programFile];
-  
-  // Spawn the Python process (use python3 if that's how you normally invoke it)
+  const scriptArgs = [operation.script];
+
+  if(operation.env){
+    scriptArgs.push('--env', operation.env);
+  }
+
+  if(operation.programFile){
+    scriptArgs.push('--programFile', operation.programFile)
+  }
+
+  console.log(scriptArgs, 'scriptArgs')
+  // Spawn the Python process
   const pythonProcess = spawn('python3', scriptArgs);
 
   // Handle output
@@ -39,10 +48,44 @@ const executePythonScript = (operation) => {
   });
 };
 
+// Function to execute a JavaScript script
+const executeJavascriptScript = (operation) => {
+  console.log(`Starting JavaScript script: ${operation.name}`);
+
+  // Prepare arguments for the JavaScript script
+  const scriptArgs = [operation.script, '--env', operation.env, '--programFile', operation.programFile];
+
+  // Spawn the Node.js process
+  const nodeProcess = spawn('node', scriptArgs);
+
+  // Handle output
+  nodeProcess.stdout.on('data', (data) => {
+    console.log(`[${operation.name}] Output: ${data.toString()}`);
+  });
+
+  // Handle errors
+  nodeProcess.stderr.on('data', (data) => {
+    console.error(`[${operation.name}] Error: ${data.toString()}`);
+  });
+
+  // Handle process exit
+  nodeProcess.on('close', (code) => {
+    console.log(`[${operation.name}] Script exited with code ${code}`);
+  });
+};
+
 // Iterate over each operation in the configuration
 config.forEach((operation) => {
   if (fs.existsSync(operation.script)) {
-    executePythonScript(operation);
+    const fileExtension = path.extname(operation.script);
+
+    if (fileExtension === '.py') {
+      executePythonScript(operation);
+    } else if (fileExtension === '.js') {
+      executeJavascriptScript(operation);
+    } else {
+      console.error(`Unsupported script type for operation: ${operation.name}`);
+    }
   } else {
     console.error(`Script not found for operation: ${operation.name}`);
   }
